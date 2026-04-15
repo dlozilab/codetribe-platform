9a682ca5c5c4f0e0e93c52c548ed10f26c671e7c
juniorSarh <sabelohgumede@gmail.com>
2026-04-15 11:32:08 +0200

    feat: update ModuleList component documentation and comment out unused React import in Users component

--- frontend/src/pages/moduleList.jsx
+++ frontend/src/pages/moduleList.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+
+// The ModuleList page will show all the modules available to the trainee. and a modal of module details when clicked on a module. 
+// If the module details are too big then we will consider a page instead.
 export default function ModuleList() {
   return (
     <div>moduleList</div>
